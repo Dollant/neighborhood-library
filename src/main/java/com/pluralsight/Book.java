a7a6e45f61a91108b5842207205ca241bbc9f2cc
Dollant <ariawilliams2425@gmail.com>
2026-04-17 11:14:28 -0400

    Added constructor method for Book initialization, initializes id, isbn, title; defaults isCheckedOut to false. This ensures new Book objects start in a consistent state.

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -7,4 +7,12 @@ public class Book {
     private String title;
     private boolean isCheckedOut;
     private String checkedOutTo;
+
+    public Book(int id, String isbn, String title) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = false;
+        this.checkedOutTo = "";
+    }
 }
