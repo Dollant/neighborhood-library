fd870fa5efa3271804a4651cc973db393c3fe337
Dollant <ariawilliams2425@gmail.com>
2026-04-17 13:22:08 -0400

    Added display for checked-out books. Lists all books currently checked out with borrower names. Supports user option to check in a book.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -44,6 +44,27 @@ public class Main {
 
             switch (homeChoice) {
 
+                case "1":
+                    System.out.println("\n₊✩‧₊˚౨ৎ˚₊✩‧₊Available Books₊✩‧₊˚౨ৎ˚₊✩‧₊");
+                    System.out.printf("%-5s %-20s %-40s%n", "ID", "ISBN", "Title");
+                    System.out.println("⊹₊˚‧︵‿₊୨ᰔ୧₊‿︵‧˚₊⊹⊹₊˚‧︵‿₊୨ᰔ୧₊‿︵‧˚₊⊹⊹₊˚‧︵‿₊୨ᰔ୧₊‿︵‧˚₊⊹");
+
+                    for (Book book : books) {
+                        if (!book.isCheckedOut()) {
+                            System.out.printf("%-5d %-20s %-40s%n",
+                                    book.getId(),
+                                    book.getIsbn(),
+                                    book.getTitle());
+                        }
+                    }
+
+                    System.out.print("\nEnter C to check in a book, or X to go back: ");
+                    String checkedOutChoice = keyboard.nextLine().trim();
+
+                    if (checkedOutChoice.equalsIgnoreCase("C")) {
+
+                    }
+
             }
         }
     }
