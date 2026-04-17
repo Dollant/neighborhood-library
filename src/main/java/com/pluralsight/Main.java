21d191a58a2be900d3d6254a2e8af34b4d3d81b9
Dollant <ariawilliams2425@gmail.com>
2026-04-17 14:22:26 -0400

    Display checked-out books.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -45,7 +45,7 @@ public class Main {
             switch (homeChoice) {
 
                 case "1":
-                    // Show available books
+
                     System.out.println("\n₊✩‧₊˚౨ৎ˚₊✩‧₊ Available Books ₊✩‧₊˚౨ৎ˚₊✩‧₊");
                     System.out.printf("%-5s %-20s %-40s%n", "ID", "ISBN", "Title");
                     System.out.println("⊹₊˚‧︵‿₊୨ᰔ୧₊‿︵‧˚₊⊹⊹₊˚‧︵‿₊୨ᰔ୧₊‿︵‧˚₊⊹⊹₊˚‧︵‿₊୨ᰔ୧₊‿︵‧˚₊⊹");
@@ -85,6 +85,21 @@ public class Main {
                     }
                     break;
 
+                case "2":
+
+                    System.out.println("\n₊✩‧₊˚౨ৎ˚₊✩‧₊ Checked Out Books ₊✩‧₊˚౨ৎ˚₊✩‧₊");
+                    System.out.printf("%-5s %-20s %-40s%n", "ID", "ISBN", "Title");
+                    System.out.println("⊹₊˚‧︵‿₊୨ᰔ୧₊‿︵‧˚₊⊹⊹₊˚‧︵‿₊୨ᰔ୧₊‿︵‧˚₊⊹⊹₊˚‧︵‿₊୨ᰔ୧₊‿︵‧˚₊⊹");
+
+                    for (Book book : books) {
+                        if (book.isCheckedOut()) {
+                            System.out.printf("%-5d %-20s %-40s%n",
+                                    book.getId(),
+                                    book.getIsbn(),
+                                    book.getTitle(),
+                                    book.getCheckedOutTo());
+                        }
+                    }
             }
         }
     }
